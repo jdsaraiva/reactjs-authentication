259104258bf9e6af4a921a1e59b4b00b0dbc3ab2
jdessainsaraiva <joaodsaraiva@gmail.com>
2019-12-19 10:30:53 +0000

    App skeleton

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import {Route} from 'react-router-dom';
+import Home from "./home";
+import Profile from "./profile";
+import Nav from "./Nav";
 
 function App() {
   return (
-    <div className="App" />
+      <>
+        <Nav />
+        <div className="body">
+          <Route path="/" exact component={Home} />
+          <Route path="/profile" component={Profile} />
+        </div>
+      </>
   );
 }
 
